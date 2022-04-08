cc5cbebbf9b10835ef16f59e8b867b9f1369767c
aayteeatul <atul.aaytee@gmail.com>
2022-04-08 23:04:56 +0200

    Added to I2C Scanner folder

---
+++ I2C_Scanner/I2C_Scanner.ino
@@ -0,0 +1,53 @@
+// This is written for the Arduino-DUE, for using this sketch in Arduino Uno/Mega, replace Wire1 with Wire and comment "extern TwoWire Wire"
+
+#include <Wire.h> //include Wire.h library
+extern TwoWire Wire1;
+
+void setup()
+{
+  Wire1.begin(); // Wire communication begin
+  Serial.begin(115200); // The baudrate of Serial monitor is set in 9600
+  while (!Serial); // Waiting for Serial Monitor
+  Serial.println("\nI2C Scanner");
+}
+
+void loop()
+{
+  byte error, address; //variable for error and I2C address
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for (address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire1.beginTransmission(address);
+    error = Wire1.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.print(address, HEX);
+      Serial.println("  !");
+      nDevices++;
+    }
+    else if (error == 4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);
+    }
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  delay(500); // wait 2 seconds for the next I2C scan
+}
