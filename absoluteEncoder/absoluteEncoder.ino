a54892e25da93f6dca758b017b56d5898e99b23f
aayteeatul <87950522+aayteeatul@users.noreply.github.com>
2022-04-11 15:41:50 +0200

    with counter
    script has both the absolute and incremental Position

--- absoluteEncoder/absoluteEncoder.ino
+++ absoluteEncoder/absoluteEncoder.ino
@@ -1,17 +1,34 @@
 #include <AS5600.h>
 AMS_5600 ams5600;
 
-float output;
+float absPos, incrPos;
+float prevA = 0;
+float currA = 0;
+int counter = 0;
 
 void setup()
 {
   Serial.begin(115200);
   Wire.begin();
-  //delay(500);
+  absPos = ams5600.getRawAngle()*(float)360/4096;
+  prevA = absPos; 
 }
 
 void loop()
 {
-  output = ams5600.getRawAngle()*(float)360/4096;
-  Serial.println(output,5);
+  absPos = ams5600.getRawAngle()*(float)360/4096;
+  currA = absPos;
+  if (abs(currA - prevA) >= 345 ) {
+    if( (currA-prevA) >= 345) {
+      counter = counter -1;  
+    }
+    if( (currA-prevA) <= -345) {
+      counter = counter + 1;  
+    }   
+  }
+  incrPos = counter*360 + absPos;
+  Serial.print(absPos,5);
+  Serial.print(" , ");
+  Serial.println(incrPos,5);
+  prevA = currA;
 }
