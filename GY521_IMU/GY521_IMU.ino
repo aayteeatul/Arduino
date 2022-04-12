c924b8b503a6a61fb805d067a6ab2f03b47e8079
aayteeatul <87950522+aayteeatul@users.noreply.github.com>
2022-04-12 12:02:46 +0200

    Reduced Code

--- GY521_IMU/GY521_IMU.ino
+++ GY521_IMU/GY521_IMU.ino
@@ -1,99 +1,37 @@
-// I2Cdev and MPU6050 must be installed as libraries
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
+// I2Cdev and MPU6050 must be installed as libraries, I have added some of the cosntants into the library..
+#include <I2Cdev.h>
+#include <MPU6050_6Axis_MotionApps20.h>
+#include <Wire.h>
 
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    #include "Wire.h"
-#endif
-
-// AD0 high = 0x69 else 0x68 (class default)
 MPU6050 mpu;
 
 #define OUTPUT_READABLE_YAWPITCHROLL
 
-#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
-
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-
-
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-    mpuInterrupt = true;
-}
-
-
-
-// ================================================================
-// ===                      INITIAL SETUP                       ===
-// ================================================================
-
 void setup() {
     Serial.begin(115200);
-    while (!Serial); // wait for Leonardo enumeration, others continue immediately
-
-    Serial.println(F("Initializing I2C devices..."));
+    while (!Serial);
     mpu.initialize();
-    pinMode(INTERRUPT_PIN, INPUT);
-
-    // verify connection
-    Serial.println(F("Testing device connections..."));
     Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-    // load and configure the DMP
-    Serial.println(F("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
-
-    // supply your own gyro offsets here, scaled for min sensitivity
+    //Set the offset for Gyro
     mpu.setXGyroOffset(102);
     mpu.setYGyroOffset(-26);
     mpu.setZGyroOffset(-11);
-    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
 
-    // make sure it worked (returns 0 if so)
     if (devStatus == 0) {
-        // Calibration Time: generate offsets and calibrate our MPU6050
-        mpu.CalibrateAccel(6);
         mpu.CalibrateGyro(6);
-        mpu.PrintActiveOffsets();
         mpu.setDMPEnabled(true);
-
-        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
-
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
         dmpReady = true;
         packetSize = mpu.dmpGetFIFOPacketSize();
     } else {
         Serial.println(F("DMP Initialization failed"));
-
     }
 }
 
 
 void loop() {
-    // if programming failed, don't try to do anything
     if (!dmpReady) return;
-    
-    // read a packet from FIFO
-    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
-        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {
             // display Euler angles in degrees
             mpu.dmpGetQuaternion(&q, fifoBuffer);
             mpu.dmpGetGravity(&gravity, &q);
@@ -104,8 +42,5 @@ void loop() {
             Serial.print(ypr[1] * 180/M_PI);
             Serial.print("\t");
             Serial.println(ypr[2] * 180/M_PI);
-        #endif
-        // blink LED to indicate activity
-
     }
 }
